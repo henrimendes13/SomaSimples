0f98ce56bf9eac5e5de61de65b85af2bebe04a1a
Henrique Mendes <henriquemendes13@gmail.com>
2023-08-20 22:13:52 -0300

    Add project files.

---
+++ SomaSimples/Program.cs
@@ -0,0 +1,7 @@
+﻿int a, b, soma;
+Console.WriteLine("Digite 2 valores para soma.");
+a = int.Parse(Console.ReadLine());
+b = int.Parse(Console.ReadLine());
+soma = a + b;
+
+Console.WriteLine("SOMA: "+soma);
